f4c1189bf6afc2f02df183eda4492313da114151
Adi Darmawan <adidrmwan17@gmail.com>
2020-11-17 23:24:44 +0700

    login, search item, add, and checkout it

--- BDD/Features/tanihub/checkout.feature
+++ BDD/Features/tanihub/checkout.feature
@@ -1,19 +1,18 @@
 Feature: Checkout the item in TaniHub
 
-    As a User, I can login and search an item (ex: Minyak Goreng Rose Brand 2 L Karton),
-    Add the item into Cart, and then Checkout it.
+    As a User
+    I can login and search an item (ex: Minyak Goreng Rose Brand 2 L Karton),
+    Add the i
+    tem into Cart, and then Checkout it.
 
-    Scenario: Login to TaniHub
-        Given I am on TaniHub Home page
-        When I Click Login Button
-        Then I See Login Page
-            And I See Email Field
-        When I Enter "testinguser@mailinator.com" to Email Field
-            And I Click Selanjutnya Button
-        Then I See "Password" Field
-        When I Enter "admin123" to Password Field
-            And I Click Masuk Button
-        Then I See "Akun Saya" in Home Page
-    
-    Scenario: Search an Item
+    Background:
+        Given I use the Chrome Browser
+            And I Navigate to TaniHub Homepage
 
+    Scenario: Search an Item and Checkout it
+        Given I am logged in as "testinguser@mailinator.com"
+        When I Enter "Minyak Goreng Rose Brand 2 L Karton" to Search Field
+        Then I See the Result of "Rose Brand Minyak Goreng 2 L Karton" on Page
+        When I Add The Item to Cart
+            And I Click Checkout Button
+        Then I See The Summary Page of Checkout
--- BDD/Features/tanihub/steps/checkoutStep.py
+++ BDD/Features/tanihub/steps/checkoutStep.py
@@ -4,60 +4,70 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+# from webdriver_manager import driver
+# from behave_webdriver import driver
 
-@given('I am on TaniHub Home page')
-def openHomePage(context):
+
+@given(u'I use the Chrome Browser')
+def LaunchBrowser(context):
     context.driver = webdriver.Chrome()
     context.driver.maximize_window()
+
+@given(u'I Navigate to TaniHub Homepage')
+def NavigateToTanihub(context):
     context.driver.get("https://tanihub.com/")
     context.driver.find_element_by_xpath("//*[@id='app-layout']/nav/div[2]/div[2]/div/div[2]/section/div/div[2]/div[1]/label").click()
     context.driver.implicitly_wait(2)
-
-@when('I Click Login Button')
-def LoginButton(context):
     context.driver.find_element_by_id("button-1").click()
     context.driver.implicitly_wait(2)
-
-@then('I See Login Page')
-def VerifyLoginPage(context):
     login_title = context.driver.find_element_by_xpath("/html/body/div[3]/div/div/div[1]/p").text
     assert (login_title == "Masuk")
     context.driver.implicitly_wait(2)
 
-@then('I See Email Field')
-def VerifyEmailField(context):
-    email_title = context.driver.find_element_by_xpath("/html/body/div[3]/div/div/div[2]/form/div[1]/label/span/i").text
-    assert (email_title == "Email Anda")
-
-@when('I Enter "{email}" to Email Field')
-def InputEmail(context,email):
+@given(u'I am logged in as "{email}"')
+def LoggedInAsUser(context,email):
     context.driver.find_element_by_xpath("//input[@type='email']").send_keys(email)
     context.driver.implicitly_wait(2)
-
-@when('I Click Selanjutnya Button')
-def ClickNextButton(context):
+    context.driver.find_element_by_xpath("//button[@type='submit']").click()
+    context.driver.implicitly_wait(2)
+    context.driver.find_element_by_id("input-password-3").send_keys('admin123')
+    context.driver.implicitly_wait(2)
     context.driver.find_element_by_xpath("//button[@type='submit']").click()
     context.driver.implicitly_wait(2)
 
-@then('I See "{password}" Field')
-def VerifyEmailField(context,password):
-    password_title = context.driver.find_element_by_xpath("/html/body/div[3]/div/div/div[2]/form/div[2]/label/p").text
-    assert (password_title == password)
-
-@when('I Enter "{password}" to Password Field')
-def InputEmail(context,password):
-    context.driver.find_element_by_id("input-password-3").send_keys(password)
+@when(u'I Enter "{product}" to Search Field')
+def SearchingProduct(context,product):
+    wait = WebDriverWait(context.driver, 10)
+    wait.until(EC.presence_of_element_located((By.XPATH, "//input[@type='text']"))).send_keys(product)
+    wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='input-icon-2']"))).send_keys(Keys.RETURN)
     context.driver.implicitly_wait(2)
 
-@when('I Click Masuk Button')
-def ClickSubmitButton(context):
-    context.driver.find_element_by_xpath("//button[@type='submit']").click()
+@then(u'I See the Result of "{result}" on Page')
+def SearchingResult(context,result):
+    wait = WebDriverWait(context.driver, 10)
+    result_text = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='app-layout']/div[1]/div/div[2]/div/div[2]/div[2]/div[1]/div[2]/p"))).text
+    assert (result_text == result)
+    context.driver.implicitly_wait(5)
+
+@when(u'I Add The Item to Cart')
+def AddItem(context):
+    context.driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[2]/div[5]/button[1]").click()
     context.driver.implicitly_wait(2)
 
-@then('I See "{account}" in Home Page')
-def VerifyMyAccount(context,account):
-    context.driver.find_element_by_xpath("//*[@id='app-layout']/nav/div[4]/div/div[2]/div/div[1]/button").click()
+@when(u'I Click Checkout Button')
+def ClickCheckout(context):
+    wait = WebDriverWait(context.driver, 10)
+    context.driver.find_element_by_xpath("//*[@id='app-layout']/nav/div[4]/button[2]").click()
     context.driver.implicitly_wait(2)
-    account_title = context.driver.find_element_by_xpath("//*[@id='dropdown-menu']/div/div[1]/span[1]/p").text
-    assert (account_title == account)
+    context.driver.find_element_by_xpath("//*[@id='app-layout']/div/div/div[3]/div[2]/div/button").click()
+    context.driver.implicitly_wait(2)
 
+@then(u'I See The Summary Page of Checkout')
+def SummaryPage(context):
+    wait = WebDriverWait(context.driver, 10)
+    address = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='app-layout']/div[2]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/p[2]"))).text 
+    print (address)
+    item = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='app-layout']/div[2]/div/div[2]/div[1]/div[3]/div/div"))).is_displayed()
+    assert item is True
+    payment = context.driver.find_element_by_xpath("//*[@id='app-layout']/div[2]/div/div[2]/div[2]/div").is_displayed()
+    assert payment is True
